7ed7556bdae6983b2a64ecb34e1372ae4bdf0345
PkcPrashant <pc22230@gmail.com>
2019-11-19 00:15:23 +0530

    Updates

---
+++ precache-manifest.2e2cbcc23b30952ccde05906e1c8cb77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a786498c56b38d248b534d4184b7f00",
+    "url": "/IN-CUBE-Login-Authentication-Page/index.html"
+  },
+  {
+    "revision": "2a9692d0a4c18c8baf7f",
+    "url": "/IN-CUBE-Login-Authentication-Page/static/css/2.fd83e43b.chunk.css"
+  },
+  {
+    "revision": "b0c868fa22d125967ae3",
+    "url": "/IN-CUBE-Login-Authentication-Page/static/css/main.50e0d94b.chunk.css"
+  },
+  {
+    "revision": "2a9692d0a4c18c8baf7f",
+    "url": "/IN-CUBE-Login-Authentication-Page/static/js/2.f12bab35.chunk.js"
+  },
+  {
+    "revision": "b0c868fa22d125967ae3",
+    "url": "/IN-CUBE-Login-Authentication-Page/static/js/main.1d7dd25c.chunk.js"
+  },
+  {
+    "revision": "e79a59f7a6f6fecdb6ba",
+    "url": "/IN-CUBE-Login-Authentication-Page/static/js/runtime-main.a2938402.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/IN-CUBE-Login-Authentication-Page/precache-manifest.2e2cbcc23b30952ccde05906e1c8cb77.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IN-CUBE-Login-Authentication-Page/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
