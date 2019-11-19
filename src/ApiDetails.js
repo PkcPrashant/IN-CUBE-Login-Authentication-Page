6ce6f3392728bef4432fcb6c12dd7a215d0b9fff
Pkc Prashant <32976692+PkcPrashant@users.noreply.github.com>
2019-11-19 14:13:13 +0530

    Update ApiDetails.js

--- src/ApiDetails.js
+++ src/ApiDetails.js
@@ -1,13 +1,12 @@
-//export const GOOGLE_CLIENT_ID = '658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com';
-export const GOOGLE_CLIENT_ID = '278205736150-prnf2ugv71kt5n7tnied504u767dlmj3.apps.googleusercontent.com';
+export const GOOGLE_CLIENT_ID = 'xxxxx';
 
-export const FACEBOOK_CLIENT_ID = '555351001882800';
+export const FACEBOOK_CLIENT_ID = 'xxxxx';
 
-export const GITHUB_CLIENT_ID = '6bac23ed722557bf5e99';
-export const GITHUB_CLIENT_SECRET = 'c48a8c89c103f96499cd5217f5fc81a2953762ea';
+export const GITHUB_CLIENT_ID = 'xxxxx';
+export const GITHUB_CLIENT_SECRET = 'xxxxx';
 
-export const LINKEDIN_CLIENT_ID = '86ogqcavau6buk';
-export const LINKEDIN_CLIENT_SECRET = 'Hk7WnOhlnROLakU5';
+export const LINKEDIN_CLIENT_ID = 'xxxxx';
+export const LINKEDIN_CLIENT_SECRET = 'xxxxx';
 
 export const REDIRECT_URI = 'https://pkcprashant.github.io/IN-CUBE-Login-Authentication-Page';
 
